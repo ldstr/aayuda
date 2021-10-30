eeabba2cec2eb956211c6c9f2413a3810a1a5dff
Dip Roy <69411261+ldstr@users.noreply.github.com>
2021-10-30 11:29:25 +1000

    Create dev_console.js

---
+++ aayuda-frontend/public/dev_console.js
@@ -0,0 +1,9 @@
+console.log(
+  "%cA'ayuda",
+  'color: #2563EB; font-weight: bold; font-size: 50px; -webkit-text-stroke: 1px black;'
+);
+
+[
+  'App Source: https://github.com/ldstr/aayuda',
+  'WARNING: https://wikipedia.org/wiki/Self-XSS'
+].forEach((i) => console.log(i));
